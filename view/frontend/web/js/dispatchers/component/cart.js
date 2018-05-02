d19302eafa6406494ce16f499584b5526b2f8d4c
Orhan Cakici <orhan.cakici@tghp.co.uk>
2018-05-02 11:42:29 +0100

    Added cart data to dispatch to events checkout_cart_item_remove and checkout_cart_update

--- view/frontend/web/js/dispatchers/component/cart.js
+++ view/frontend/web/js/dispatchers/component/cart.js
@@ -1,85 +1,84 @@
 define([
-	'jquery',
-	'underscore',
-	'frontendEventManager'
+    'jquery',
+    'underscore',
+    'frontendEventManager'
 ], function (
-	$,
-	_,
-	eventManager
+    $,
+    _,
+    eventManager
 ) {
 
-	function getCartQtyFingerprint(qtys) {
-		return qtys.join('');
-	}
+    function getCartQtyFingerprint(qtys) {
+        return qtys.join('');
+    }
 
-	function getCartQtys($cart) {
-		return $cart.find('[name$="[qty]"]')
-			.map(function () { return $(this).val() })
-			.toArray();
-	}
+    function getCartQtys($cart) {
+        return $cart.find('[name$="[qty]"]')
+            .map(function () { return $(this).val() })
+            .toArray();
+    }
 
-	return function(config, element) {
+    return function(config, element) {
 
-		var $cart = $(element),
-			initialQtyFingerprint = getCartQtyFingerprint(getCartQtys($cart));
+        var $cart = $(element),
+            initialQtyFingerprint = getCartQtyFingerprint(getCartQtys($cart));
 
-		console.log('bound to', $cart);
+        console.log('bound to', $cart);
 
-		$cart.data('cart-qty-hash-initial', initialQtyFingerprint);
-		$cart.data('cart-qty-hash', initialQtyFingerprint);
+        $cart.data('cart-qty-hash-initial', initialQtyFingerprint);
+        $cart.data('cart-qty-hash', initialQtyFingerprint);
 
-		$cart.on('change', '[name$="[qty]"]', function () {
-			eventManager.dispatchEvent(
-				'checkout_cart_update_item_input',
-				{
-					form: $cart,
-					input: $(this)
-				}
-			);
-			$cart.data('cart-qty-hash', getCartQtyFingerprint(getCartQtys($cart)));
-		});
+        $cart.on('change', '[name$="[qty]"]', function () {
+            eventManager.dispatchEvent(
+                'checkout_cart_update_item_input',
+                {
+                    form: $cart,
+                    input: $(this)
+                }
+            );
+            $cart.data('cart-qty-hash', getCartQtyFingerprint(getCartQtys($cart)));
+        });
 
-		$cart.on('click', '.action-delete', function (e) {
-			var $this = $(this);
+        $cart.on('click', '.action-delete', function (e) {
+            var $this = $(this);
 
-			if($this.data('eventmanager-allow-event')) {
-				$this.data('eventmanager-allow-event', false);
-			} else {
-				e.stopImmediatePropagation();
-				e.preventDefault();
+            if($this.data('eventmanager-allow-event')) {
+                $this.data('eventmanager-allow-event', false);
+            } else {
+                e.stopImmediatePropagation();
+                e.preventDefault();
 
-				var dataPost = $this.data('post') ? $this.data('post').data : {};
+                eventManager.dispatchEvent(
+                    'checkout_cart_item_remove',
+                    {
+                        form: $this.data('post') ? $this.data('post').data : {},
+                        cart: customerData.get('cart')()
+                    }
+                ).done(function () {
+                    $this.data('eventmanager-allow-event', true)
+                        .click();
+                });
+            }
+        });
 
-				// TODO: get cart item from customerData('cart')
-				eventManager.dispatchEvent(
-					'checkout_cart_item_remove',
-					{
-						deleteActionData: dataPost
-					}
-				).done(function () {
-					$this.data('eventmanager-allow-event', true)
-						.click();
-				});
-			}
-		});
+        $cart.on('submit.eventManagerCore', function (e) {
+            e.preventDefault();
 
-		$cart.on('submit.eventManagerCore', function (e) {
-			e.preventDefault();
+            if($cart.data('cart-qty-hash') !== $cart.data('cart-qty-hash-initial')) {
+                eventManager.dispatchEvent(
+                    'checkout_cart_update',
+                    {
+                        form: $cart,
+                        cart: customerData.get('cart')()
+                    }
+                ).done(function () {
+                    $cart.off('submit.eventManagerCore')
+                        .submit();
+                });
+            }
 
-			if($cart.data('cart-qty-hash') !== $cart.data('cart-qty-hash-initial')) {
-				eventManager.dispatchEvent(
-					'checkout_cart_update',
-					{
-						form: $cart
-					}
-				).done(function () {
-					$cart.off('submit.eventManagerCore')
-						.submit();
-				});
-			}
+        });
 
-		});
-
-	};
+    };
 
 });
